0ca1680019e4f6bc438a3c02bda177d481cab007
Ghazal Ehsan <ghazaltaimur27@gmail.com>
2024-10-30 18:34:51 +0400

    test

--- Routes/User.js
+++ Routes/User.js
@@ -1,9 +1,11 @@
 const FileController = require("../Controllers/FileController")
 const UserController = require("../Controllers/UserController")
+const express = require("express")
 
 module.exports = function (app, authMiddleware, validators) {
     app.post(
         "/api/login", 
+        express.static(path.join(__dirname, 'uploads')),
         UserController.login
     ),
     app.post(
